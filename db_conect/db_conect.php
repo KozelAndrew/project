d1254140361687fbdb4345d3bd8d85a776f52975
KozelAndrew <adrew.kozel>
2019-02-27 21:59:53 +0200

    База даних на віддаленому сервері

--- db_conect/db_conect.php
+++ db_conect/db_conect.php
@@ -1,5 +1,5 @@
 <?php
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'chat');
-    define('DB_PASSWORD', 'password');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'veriga13');
     define('DB_NAME', 'db_chat');
