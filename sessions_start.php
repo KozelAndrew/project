8c7df8e73ea711cf51a8000709425914aebabdb6
KozelAndrew <adrew.kozel>
2019-02-27 19:56:52 +0200

    Оголошуємо початок сесії, $_SESSION['user_id'] індентифікатор  авторизованого користувача. $_COOKIE['username'] - ім'я користувача

--- sessions_start.php
+++ sessions_start.php
@@ -1,7 +1,9 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: wikingwar
-     * Date: 27.02.19
-     * Time: 19:50
-     */
+    session_start();
+    if (!isset($_SESSION['user_id'])) {
+        if (isset($_COOKIE['user_id']) && isset($_COOKIE['username'])) {
+            $_SESSION['user_id'] = $_COOKIE['user_id'];
+            $_SESSION['username'] = $_COOKIE['username'];
+        }
+    }
+?>
