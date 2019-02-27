bdea7f567fca8d3046cdb393d1f9108dbb8543ce
Andrew <47947050+kozelandrew@users.noreply.github.com>
2019-02-27 21:15:19 +0200

    Футер

---
+++ footer.php
@@ -0,0 +1,12 @@
+</div>
+<footer class="footer">
+    <div class="logo">
+        <span>- _ -</span>
+    </div>
+    <div class="footer_content">
+        <p class="footer_text">Всі права захищені</p>
+    </div>
+</footer>
+</div>
+</body>
+</html>
