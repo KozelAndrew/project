385a68ee34856c97cbd4173ec17fe7fe61089bf0
Andrew <47947050+kozelandrew@users.noreply.github.com>
2019-02-27 19:59:17 +0200

    Формуємо головну сторінку $title - містить заголовок сторінки

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
+<?php
+    require_once('sessions_start.php');
+    $title = 'Головна';
+    require_once('nav.php');
+?>
 
+    <p>Вітаємо в нашому чаті</p>
+
+<?php
+    require_once('footer.php');
+?>
