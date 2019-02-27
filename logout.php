23d7ab13b519232c8cb3a31471b9dd8af3da7085
Andrew <47947050+kozelandrew@users.noreply.github.com>
2019-02-27 21:14:17 +0200

    Завершення сесії користувача. Супер глобальна змінна набуває значення пустого масива. При перевірці їснування файлів кукі. Якщо вони їстують, то видаляємо завжасно. $home_url - приймає значення адреси домашної сторінки.

--- logout.php
+++ logout.php
@@ -1,7 +1,17 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: wikingwar
-     * Date: 27.02.19
-     * Time: 19:49
-     */
+    session_start();
+    if (isset($_SESSION['user_id'])) {
+        $_SESSION = array();
+
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), '', time() - 3600);
+        }
+        session_destroy();
+    }
+
+    setcookie('user_id', '', time() - 3600);
+    setcookie('username', '', time() - 3600);
+
+    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
+    header('Location: ' . $home_url);
+?>
